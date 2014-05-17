6f644adc81cb1e5a942185427e2816c7f352320a
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-05-18 01:10:28 +0300

    Full main layout rewrite: add headers (first row with dates) freezing

---
+++ src/ua/kpi/teacherjournal/ScrollSynchronized.scala
@@ -0,0 +1,32 @@
+package ua.kpi.teacherjournal
+
+import android.widget.{HorizontalScrollView, ScrollView}
+import org.scaloid.common.{SHorizontalScrollView, TraitViewGroup, SScrollView}
+import android.view.View
+
+
+class SScrollViewSynchronized(view: => ScrollView, disableScrollBar: Boolean = false)
+  (implicit context: android.content.Context, parentVGroup: TraitViewGroup[_] = null)
+  extends SScrollView {
+
+  setOverScrollMode(View.OVER_SCROLL_NEVER)
+  verticalScrollBarEnabled = !disableScrollBar
+
+  override def onScrollChanged(w: Int, h: Int, oldw: Int, oldh: Int) = {
+    super.onScrollChanged(w, h, oldw, oldh)
+    view.scrollTo(w, h)
+  }
+}
+
+class SHorizontalScrollViewSynchronized(view: => HorizontalScrollView, disableScrollBar: Boolean = false)
+  (implicit context: android.content.Context, parentVGroup: TraitViewGroup[_] = null)
+  extends SHorizontalScrollView {
+  setOverScrollMode(View.OVER_SCROLL_NEVER)
+
+  horizontalScrollBarEnabled = !disableScrollBar
+
+  override def onScrollChanged(w: Int, h: Int, oldw: Int, oldh: Int) = {
+    super.onScrollChanged(w, h, oldw, oldh)
+    view.scrollTo(w, h)
+  }
+}
--- src/ua/kpi/teacherjournal/TableFragment.scala
+++ src/ua/kpi/teacherjournal/TableFragment.scala
@@ -4,12 +4,13 @@ import android.app.{Fragment, FragmentManager}
 import android.content.Context
 import android.graphics.Color._
 import android.os.Bundle
-import android.view.{View, Gravity, ViewGroup, LayoutInflater}
+import android.view._
 import android.widget.AdapterView
 import org.scaloid.common._
 import scala.language.postfixOps
 import ua.kpi.teacherjournal.Journal._
 import android.graphics.drawable.GradientDrawable
+import scala.collection.mutable.ArrayBuffer
 
 object TableFragment {
   val headerColor = rgb(0xe7, 0xe7, 0xe7)
@@ -18,7 +19,7 @@ object TableFragment {
   val bgColor = rgb(0xcc, 0xcc, 0xcc)
   val absentBackgroundColor = rgb(0xFF, 0xE5, 0xE6)
 
-  def marginRight(implicit context: Context) = 1 dip
+  def marginLeft(implicit context: Context) = 1 dip
   def marginBottom(implicit context: Context) = 1 dip
 
   def update(fragmentManager: FragmentManager, selectedSheet: Sheet) =
@@ -40,101 +41,140 @@ class TableFragment extends Fragment with RichFragment {
     require(ctx != null)
 
     val sheet = arg[Sheet]("sheet")
-
-    new SScrollView {
+    new SVerticalLayout {
+      <<.fill
       backgroundColor = bgColor
+      style {
+        case t: STextView => t.textColor(BLACK)
+          .textSize(18 dip)
+          .maxLines(1)
+          .padding(20 dip, 10 dip, 20 dip, 10 dip)
+      }
+
+      var groupSpinner: SSpinner = _
+      var cellsHScrollView, headersScrollView: SHorizontalScrollView = _
+      val rowLayouts = ArrayBuffer[STableRow]()
+      val cellViews = ArrayBuffer[ArrayBuffer[STextView]]()
+
       this += new SLinearLayout {
-        style {
-          case t: STextView => t.textColor(BLACK)
-            .textSize(18 dip)
-            .maxLines(1)
-            .padding(20 dip, 10 dip, 20 dip, 10 dip)
-        }
-
-        this += new STableLayout {
-          style {
-            case t: TraitView[_] => t.backgroundColor(headerColor).<<
-              .marginRight(marginRight)
-              .marginBottom(marginBottom)
-              .>>
+        groupSpinner = SSpinner()
+          .backgroundColor(headerColor)
+          .<<.wrap
+          .marginLeft(marginLeft).marginBottom(marginBottom)
+          .>>
+          .adapter(SArrayAdapter(selectedCourse.sheets.map(_.name).toArray)
+          .dropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+          .style(_.textColor(BLACK)
+          .textSize(18 dip)
+          .maxLines(1)
+          .padding(20 dip, 2 dip, 20 dip, 3 dip))
+        ).selection(sheetId).onItemSelected((_: AdapterView[_], _: View, pos: Int, _: Long) => {
+          if (sheetId != pos) {
+            sheetId = pos
+            update(getFragmentManager, selectedSheet)
           }
+        })
 
-          SSpinner().adapter(SArrayAdapter(selectedCourse.sheets.map(_.name).toArray)
-            .dropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            .style(_.textColor(BLACK)
-              .textSize(18 dip)
-              .maxLines(1)
-              .padding(20 dip, 2 dip, 20 dip, 3 dip))
-            ).selection(sheetId).onItemSelected((_: AdapterView[_], _: View, pos: Int, _: Long) => {
-              if (sheetId != pos) {
-                sheetId = pos
-                update(getFragmentManager, selectedSheet)
-              }
-            })
-          for ((student, studentIndex) <- sheet.students.zipWithIndex) {
-            val tv = STextView(s"${studentIndex + 1}. ${student.name}")
-            if (student.isBoss) tv.textColor = bossColor
-          }
-        }
-
-        this += new SHorizontalScrollView {
-          this += new STableLayout {
-
-            // Header row
-            this += new STableRow {
-
-              style {
-                case t: STextView => t.gravity(Gravity.CENTER_HORIZONTAL)
-                  .backgroundColor(headerColor).<<
-                  .marginRight(marginRight).marginBottom(marginBottom)
-                  .>>
-              }
-              for (column <- sheet.columns)
-                STextView(column)
-            }
-
-            // Student rows
-            var selectedCell: Option[(STextView, Record)] = None
-            for ((student, studentIndex) <- sheet.students.zipWithIndex) {
-              this += new STableRow {
-                for (record <- student.records) {
-                  style {
-                    case t: STextView => t.gravity(Gravity.CENTER_HORIZONTAL)
-                      .<<
-                      .marginRight(marginRight)
+        headersScrollView = new SHorizontalScrollViewSynchronized(cellsHScrollView, disableScrollBar = true) {
+          this += new SLinearLayout {
+            // Headers (e.g. dates)
+            for ((column, colIndex) <- sheet.columns.zipWithIndex) {
+              val header = new STextView(column) {
+                override def onMeasure(w: Int, h: Int) = {
+                  super.onMeasure(w, h)
+                  for ((cell, rowIndex) <- cellViews.map(_(colIndex)).zipWithIndex) {
+                    val layout = rowLayouts(rowIndex)
+                    cell
+                      .<<(measuredWidth, WRAP_CONTENT)(new layout.LayoutParams(_))
+                      .marginLeft(marginLeft)
                       .marginBottom(marginBottom)
-                      .>>
-                  }
-
-                  def cellBgColor(record: Record) = record match {
-                    case AbsentRecord => absentBackgroundColor
-                    case _ => cellColor
-                  }
-
-                  val cellText = record match {
-                    case GradeRecord(grade) => grade.toString
-                    case _ => ""
-                  }
-
-                  val cellView = STextView(cellText)
-                    .backgroundColor(cellBgColor(record))
-
-                  cellView.onClick {
-                    selectedCell match {
-                      case Some((cell, rec)) => cell.backgroundColor(cellBgColor(rec))
-                      case None =>
-                    }
-                    val d = new GradientDrawable()
-                    d.setStroke(2 dip, rgb(0x4d, 0x93, 0xc3))
-                    d.setColor(cellBgColor(record))
-                    cellView.backgroundDrawable = d
-                    selectedCell = Some(cellView, record)
                   }
                 }
-              }
+              }.backgroundColor(headerColor)
+               .<<
+               .marginLeft(marginLeft)
+               .marginBottom(marginBottom)
+               .>>
+              this += header
             }
           }
         }
+        this += headersScrollView
+      }
+
+      this += new SLinearLayout {
+        var cellsScrollView: SScrollView = null
+        val namesScrollView = new SScrollViewSynchronized(cellsScrollView, disableScrollBar = true) {
+           this += new SVerticalLayout {
+            <<.wrap.>>
+
+            override def onMeasure(w: Int, h: Int) = {
+              super.onMeasure(w, h)
+              groupSpinner.<<(measuredWidth, WRAP_CONTENT).>>
+            }
+
+            // student names
+            for ((student, studentIndex) <- sheet.students.zipWithIndex) {
+              val tv = STextView(s"${studentIndex + 1}. ${student.name}")
+                .backgroundColor(headerColor)
+                .<<
+                .marginLeft(marginLeft)
+                .marginBottom(marginBottom)
+                .>>
+              if (student.isBoss) tv.textColor = bossColor
+            }
+          }
+        }
+
+        cellsScrollView = new SScrollViewSynchronized(namesScrollView) {
+          cellsHScrollView = new SHorizontalScrollViewSynchronized(headersScrollView) {
+            this += new STableLayout {
+              // Student marks (main table)
+              var selectedCell: Option[(STextView, Record)] = None
+              for ((student, studentIndex) <- sheet.students.zipWithIndex) {
+                cellViews += ArrayBuffer()
+                val rowView = new STableRow {
+                  for (record <- student.records) {
+                    style {
+                      case t: STextView => t.gravity(Gravity.CENTER_HORIZONTAL)
+                    }
+
+                    def cellBgColor(record: Record) = record match {
+                      case AbsentRecord => absentBackgroundColor
+                      case _ => cellColor
+                    }
+
+                    val cellText = record match {
+                      case GradeRecord(grade) => grade.toString
+                      case _ => ""
+                    }
+
+                    val cellView = STextView(cellText)
+                      .backgroundColor(cellBgColor(record))
+
+                    cellView.onClick {
+                      selectedCell match {
+                        case Some((cell, rec)) => cell.backgroundColor(cellBgColor(rec))
+                        case None =>
+                      }
+                      val d = new GradientDrawable()
+                      d.setStroke(2 dip, rgb(0x4d, 0x93, 0xc3))
+                      d.setColor(cellBgColor(record))
+                      cellView.backgroundDrawable = d
+                      selectedCell = Some(cellView, record)
+                    }
+                    cellViews.last += cellView
+                  }
+                }
+                this += rowView
+                rowLayouts += rowView
+              }
+            }
+          }
+          this += cellsHScrollView
+        }
+        this += namesScrollView
+        this += cellsScrollView
       }
     }
   }
