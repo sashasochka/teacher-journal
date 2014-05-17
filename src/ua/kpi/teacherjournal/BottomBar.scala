eecfd379448948141fb92dd56146da03876900e5
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-05-17 21:13:47 +0300

    Add progress-bar below "until class end" message in bottom bar

--- src/ua/kpi/teacherjournal/BottomBar.scala
+++ src/ua/kpi/teacherjournal/BottomBar.scala
@@ -1,14 +1,13 @@
 package ua.kpi.teacherjournal
 
+import android.app._
 import android.content.Context
 import android.graphics.Color._
 import android.os.Bundle
-import android.view.{Gravity, ViewGroup, LayoutInflater}
+import android.view._
 import org.scaloid.common._
-import scala.language.postfixOps
-import android.app.{Fragment, FragmentManager}
 import RandData._
-import Word.correctForm
+import scala.language.postfixOps
 import ua.kpi.teacherjournal.Journal.HourMinute
 
 object BottomBar {
@@ -22,31 +21,57 @@ object BottomBar {
 class BottomBar extends Fragment with RichFragment {
   def untilClassEndText(implicit ctx: Context) = timeUntilClassEnd match {
       case Some(HourMinute(hours, minutes)) =>
-        import R.{string => RS}
-        if (isLandscapeOrientation) {
-          val hrs = if (hours == 0) ""
-            else s"$hours ${correctForm(hours, RS.hour, RS.hours_paucal, RS.hours)} "
-          val mins = if (minutes == 0) ""
-            else minutes + " " + correctForm(minutes, RS.minute, RS.minutes_paucal, RS.minutes)
-          R.string.until_class_end.r2String + ": " + hrs + mins
-        } else {
-          R.string.until_class_end.r2String + ": %01d:%02d".format(hours, minutes)
-        }
+        R.string.until_class_end.r2String + " %01d:%02d".format(hours, minutes)
       case None => ""
     }
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle) = {
-    require(ctx != null)
+    assert(ctx != null)
     new SRelativeLayout {
       backgroundColor = rgb(0x1D, 0x2E, 0x35)
-      val untilClassEnd = STextView()
-        .textSize(18 dip)
-        .textColor(WHITE)
-        .gravity(Gravity.CENTER_VERTICAL)
-        .padding(0, 0, 25 dip, 0)
-        .<<(WRAP_CONTENT, MATCH_PARENT)
+      val rightBlock = new SVerticalLayout {
+        padding(0, 0, 25 dip, 0)
+        gravity = Gravity.CENTER_VERTICAL
+        val untilClassEnd = STextView()
+          .textSize(18 dip)
+          .padding(0, 0, 0, 5 dip)
+          .textColor(WHITE)
+          .gravity(Gravity.CENTER_VERTICAL)
+
+
+        this += new SLinearLayout {
+          val progressBarHeight = 3 dip
+          val positive = STextView()
+            .backgroundColor(rgb(0x78, 0x9c, 0x24)).<<(FILL_PARENT, progressBarHeight).Weight(1.0f).>>
+          val negative = STextView()
+            .backgroundColor(rgb(0xd1, 0x3d, 0x3d)).<<(FILL_PARENT, progressBarHeight).Weight(1.0f).>>
+
+          def updateView(): Unit = {
+              if (ctx != null) {
+                untilClassEnd.text = untilClassEndText
+
+                timeUntilClassEnd match {
+                  case Some(HourMinute(h, m)) =>
+                    positive.visibility(View.VISIBLE)
+                    negative.visibility(View.VISIBLE)
+                    val q = (h * 60 + m) / 95.0f
+                    positive.<<(FILL_PARENT, progressBarHeight).Weight(q)
+                    negative.<<(FILL_PARENT, progressBarHeight).Weight(1.0f - q)
+                  case None =>
+                    positive.visibility(View.INVISIBLE)
+                    negative.visibility(View.INVISIBLE)
+                }
+                postDelayed(updateView(), 1000)
+              }
+          }
+          updateView()
+        }
+      }
+      this += rightBlock
+        .<<(WRAP_CONTENT, FILL_PARENT)
         .alignParentRight
         .>>
+
       val lastSync = STextView(s"${R.string.last_sync.r2String}: $randomTime")
         .textSize(14 dip)
         .gravity(Gravity.CENTER_VERTICAL)
@@ -57,13 +82,6 @@ class BottomBar extends Fragment with RichFragment {
         .<<(WRAP_CONTENT, MATCH_PARENT)
         .alignParentLeft
         .>>
-      def updateTime(): Unit = {
-        if (ctx != null) {
-          untilClassEnd.text(untilClassEndText)
-          postDelayed(updateTime(), 1000)
-        }
-      }
-      updateTime()
     }
   }
 }
